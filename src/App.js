a38a6fe97a2be0f565f50102a1bdb20385bf7cb0
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-03-17 18:13:33 +0100

    Todo[1]
    Design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,68 @@
 import React from "react";
 
-export default function App(){
+export default function App() {
+  return (
+    <div className="app">
+      <div className="nav">
+        <div className="title-container">
+          <h1 className="title">TO DO list</h1>
+          <div className="img-container">
+          <img src="./plus.png" className="img"/>
 
-    return(
-        <h1>sfaasfas</h1>
-    )
+          </div>
+        </div>
+        <form>
+            <div className="form-item">
+            <input type="text" className="inputs duty-input" placeholder="Duty"/>
+            <input type="text" className="inputs tags-input" placeholder="Tags"/>
+            </div>
+        <div>
+            <input type="text" className="inputs desc-input" placeholder="Description"/>
+        </div>
+
+        </form>
+      </div>
+      <div className="container">
+        <div className="description">
+          <h1>TO do</h1>
+        </div>
+        <div className="list">
+          <div>
+            <div className="todos to-do">
+              <span className="item">Duty</span>
+              <span className="item">Description</span>
+              <span className="item">Tags</span>
+              <span className="item">Finished</span>
+            </div>
+          </div>
+
+          <div>
+            <div className="description">
+              <h1>In progress</h1>
+            </div>
+
+            <div className="todos in-progress">
+              <span className="item">Duty</span>
+              <span className="item">Description</span>
+              <span className="item">Tags</span>
+              <span className="item">Finished</span>
+            </div>
+          </div>
+
+          <div>
+            <div className="description">
+              <h1>Finished</h1>
+            </div>
+
+            <div className="todos finished">
+              <span className="item">Duty</span>
+              <span className="item">Description</span>
+              <span className="item">Tags</span>
+              <span className="item">Finished</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
